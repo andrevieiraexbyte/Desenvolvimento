525056cce958da352dac2475a3d83b013054018c
André Vieira <andrevieira_@msn.com>
2018-02-14 20:21:14 -0200

    utilizando strings

---
+++ sintaxe-variaveis-e-fluxo/src/TestaCaracteres.java
@@ -0,0 +1,16 @@
+
+public class TestaCaracteres {
+
+		public static void main(String[] args) {
+			
+			// utulizando caractéries
+			
+			char letra = 'a'; // utilizado para unicode
+			
+			String frase = "string não é um tipo, não é uma palavra reservada do java";
+			
+			String nome = "André Vieira";
+			
+			System.out.println(frase+"\n"+nome);
+		}
+}
