7c5dbc59a3c0f45ea71995641b115f24e860d06e
André Vieira <andrevieira_@msn.com>
2018-02-14 20:02:18 -0200

    utilzando conversão, cast e forma literal

---
+++ sintaxe-variaveis-e-fluxo/src/TestaConversao.java
@@ -0,0 +1,17 @@
+
+public class TestaConversao {
+	
+	public static void main(String[] args) {
+		
+		double salario = 1270.70;
+		
+		
+		
+		int valor = (int) salario; // cast fazendo converssão de double para int
+		System.out.println(valor);
+		
+		float outroValor = 15.54f; // declarando de forma literal que é float utilizando o f no final do número
+		System.out.println(outroValor);
+	
+	}
+}
