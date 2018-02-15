c572398203ba84883840974808509c2fa4c6d86c
André Vieira <andrevieira_@msn.com>
2018-02-15 16:18:59 -0200

    calculando gasto trimestral sintaxe básica

---
+++ sintaxe-variaveis-e-fluxo/src/BalancoTrimestral.java
@@ -0,0 +1,15 @@
+
+public class BalancoTrimestral {
+
+	public static void main(String[] args) {
+		
+		int gastosJaneiro = 15000;
+		int gastosFevereiro = 23000;
+		int gastosMarco = 17000;
+		
+		int gastroTrimestral = gastosJaneiro + gastosFevereiro + gastosMarco;
+		
+		System.out.println("o total do gasto trimestral é: "+gastroTrimestral);
+	}
+
+}
