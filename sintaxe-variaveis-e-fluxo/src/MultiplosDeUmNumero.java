1c9a25b3a1678995b0b643a579d779b0d2c6f490
André Vieira <andrevieira_@msn.com>
2018-02-14 22:51:19 -0200

    multiplos de um número

---
+++ sintaxe-variaveis-e-fluxo/src/MultiplosDeUmNumero.java
@@ -0,0 +1,14 @@
+
+public class MultiplosDeUmNumero {
+
+	public static void main(String[] args) {
+
+		int numero = 3;
+
+		for (int i = numero; i <= 100; i += numero) {
+			System.out.print(i + " ");
+		}
+
+	}
+
+}
