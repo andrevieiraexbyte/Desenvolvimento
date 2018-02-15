a13164bcbc81be46e4ca79ac2dedff39174975e4
André Vieira <andrevieira_@msn.com>
2018-02-15 16:52:55 -0200

    utilizando for, while, e ifs

--- sintaxe-variaveis-e-fluxo/src/BalancoTrimestral.java
+++ sintaxe-variaveis-e-fluxo/src/BalancoTrimestral.java
@@ -2,17 +2,52 @@
 public class BalancoTrimestral {
 
 	public static void main(String[] args) {
-		
+
 		int gastosJaneiro = 15000;
 		int gastosFevereiro = 23000;
 		int gastosMarco = 17000;
-		
-		int gastroTrimestral = gastosJaneiro + gastosFevereiro + gastosMarco;
-		
-		int mediaTrimestral = gastroTrimestral / 3;
-		
-		System.out.println("o total do gasto trimestral é: "+gastroTrimestral);
-		System.out.println("o total do gasto trimestral é: "+mediaTrimestral);
-	}
 
+		int gastroTrimestral = gastosJaneiro + gastosFevereiro + gastosMarco;
+
+		int mediaTrimestral = gastroTrimestral / 3;
+
+		System.out.println("o total do gasto trimestral é: " + gastroTrimestral);
+		System.out.println("o total do gasto trimestral é: " + mediaTrimestral);
+
+		// criando um laço com while
+
+		int num1 = 150;
+		while (num1 < 300) {
+			System.out.println(num1);
+			num1++;
+		}//fazendo somatoria de todos os numeros
+		int soma = 0;
+		int num2 = 1;
+		while (num2 <= 1000) {
+			soma = soma + num2;
+			num2++;
+			System.out.println(soma);
+		}
+
+		for (int i = 1; i <= 1000; i++) {
+			soma = soma + i;
+		}
+		System.out.println("O valor da soma e = " + soma);
+		// multiplos de 3
+		int multiplo = 3;
+		for ( int i = 1 ; i <= 10; i++) {
+			System.out.println(multiplo*i);
+			
+		}// multiplos de 3
+		 for (int i = 3; i < 100; i += 3 ){
+	            System.out.println(i);
+	        }
+		
+		 int x = 11;
+		 if(x % 2 == 0  ) {
+			 System.out.println("numero passado é par!");
+		 }else {
+			 System.out.println("numero passdo e ímpar!");
+		 }
+	}
 }
--- sintaxe-variaveis-e-fluxo/src/Fatorial.java
+++ sintaxe-variaveis-e-fluxo/src/Fatorial.java
@@ -3,9 +3,9 @@ public class Fatorial {
 
 	public static void main(String[] args) {
 
-		int fatorial = 1;
+		long fatorial = 1;
 
-		for (int i = 1; i < 11; i++) {
+		for (long i = 1; i < 11; i++) {
 			fatorial *= i;
 			System.out.println("fatorial de " + i + "=" + fatorial);
 		}
