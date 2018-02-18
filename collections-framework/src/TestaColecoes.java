0cd097a44b970a3d1a149e942d00a5fc619043d0
André Vieira <andrevieira_@msn.com>
2018-02-18 15:39:55 -0300

    percorrendo todo array e imprimindo com for e foreach

--- collections-framework/src/TestaColecoes.java
+++ collections-framework/src/TestaColecoes.java
@@ -4,12 +4,23 @@ public class TestaColecoes {
 
 	public static void main(String[] args) {
 
-		ArrayList nomes = new ArrayList();
+		ArrayList<String> nomes = new <String>ArrayList(); // so aceita string <String>
 
 		nomes.add("André Vieira");// adicionando no array
 		nomes.add("Abel");
 
 		System.out.println(nomes.get(1));// acessando a posição no array
+
+		System.out.println(nomes.contains("André Vieira"));// passando elemento pra saber se contém na lista
+
+		for (int i = 0; i < nomes.size(); i++) {// imprimindo todos dentro do arraylist
+
+			System.out.println(nomes.get(i));
+
+		}
+		for (String nome : nomes) {// passando por todos elemtnos usando o foreach
+			System.out.println(nome);
+		}
 
 	}
 
