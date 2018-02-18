cd5196578482c99da4a5aa28c56a98930911b738
André Vieira <andrevieira_@msn.com>
2018-02-18 15:28:14 -0300

    usando collection ArrayList

---
+++ collections-framework/src/TestaColecoes.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+
+public class TestaColecoes {
+
+	public static void main(String[] args) {
+
+		ArrayList nomes = new ArrayList();
+
+		nomes.add("André Vieira");// adicionando no array
+		nomes.add("Abel");
+
+		System.out.println(nomes.get(1));// acessando a posição no array
+
+	}
+
+}
