d4d85b90ad331806fe5061e391bae97b4b020fd3
André Vieira <andrevieira_@msn.com>
2018-02-24 18:09:30 -0300

    criando um novo imposto ICCC com uma nova regra de negócio

--- design-patterns/src/strategy/TesteDeImpostos.java
+++ design-patterns/src/strategy/TesteDeImpostos.java
@@ -6,13 +6,15 @@ public class TesteDeImpostos {// classe de teste do sistema
 
 		Imposto iss = new ISS();
 		Imposto icms = new ICMS();
+		Imposto iccc = new ICCC();
 
-		Orcamento orcamento = new Orcamento(500);
+		Orcamento orcamento = new Orcamento(3050);
 
 		CalculadoraDeImpostos calculador = new CalculadoraDeImpostos();
 
 		calculador.realizaCalculo(orcamento, iss);
 		calculador.realizaCalculo(orcamento, icms);
+		calculador.realizaCalculo(orcamento, iccc);
 	}
 
 }
