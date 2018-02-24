257c6b7fe56af65f82271a4bdf513dd8d50933f9
André Vieira <andrevieira_@msn.com>
2018-02-23 21:45:55 -0300

    utilizando principios de ocp e dip

---
+++ SOLID/src/Compra.java
@@ -0,0 +1,15 @@
+
+public class Compra {
+
+	private String cidade;
+	private double valor;
+
+	public String getCidade() {
+		return cidade;
+	}
+
+	public double getValor() {
+		return valor;
+	}
+
+}
---
+++ SOLID/src/ServicoDeEntrega.java
@@ -0,0 +1,5 @@
+
+public interface ServicoDeEntrega {
+
+	double para(String cidade);
+}
