265b443ae64c113d3a268c029ef1b8b9042c99bf
André Vieira <andrevieira_@msn.com>
2018-02-23 23:01:31 -0300

    deixando de utilizar herança e favorecendo a composição

---
+++ SOLID/src/ContaDeEstudante.java
@@ -0,0 +1,17 @@
+public class ContaDeEstudante extends ContaComum {
+
+	private int milhas;
+
+	public void deposita(double valor) {
+		super.deposita(valor);
+		this.milhas += (int) valor;
+	}
+
+	public int getMilhas() {
+		return milhas;
+	}
+
+	public void rende() {
+		throw new RuntimeException("Não pode render");
+	}
+}
