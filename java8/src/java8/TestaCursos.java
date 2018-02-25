04cd36756742b4fc5808e27e3cdef90b66496147
André Vieira <andrevieira_@msn.com>
2018-02-25 19:57:27 -0300

    ordenando lista com lambda e metodo references

---
+++ java8/src/java8/TestaCursos.java
@@ -0,0 +1,23 @@
+package java8;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+public class TestaCursos {
+
+	public static void main(String[] args) {
+
+		List<Curso> cursos = new ArrayList<>();
+
+		cursos.add(new Curso("Python", 45));
+		cursos.add(new Curso("JavaScript", 150));
+		cursos.add(new Curso("Java 8", 113));
+		cursos.add(new Curso("C", 55));
+
+		cursos.sort(Comparator.comparing(c -> c.getAlunos()));// com lambda
+		cursos.sort(Comparator.comparing(Curso::getAlunos));// com metodo reference
+
+	}
+
+}
