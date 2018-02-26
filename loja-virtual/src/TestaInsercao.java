978af1df38ac9b0812290c92b1229e836cc04cd4
André Vieira <andrevieira_@msn.com>
2018-02-26 17:00:03 -0300

    utilizando Preparedstatement contra sqlInjections

--- loja-virtual/src/TestaInsercao.java
+++ loja-virtual/src/TestaInsercao.java
@@ -1,28 +1,37 @@
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
 public class TestaInsercao {
 
+	
+
 	public static void main(String[] args) throws SQLException {
 		Connection connection = Database.getConnection();// abrindo uma conexao.
 
+		String nome = "Notebook";
+		String descricao = "Notebook Gamming i7";
 		// criando um statement
 		Statement statement = connection.createStatement();
 		// executando um instrução sql.
-		boolean resultado = statement.execute(
-				"insert into Produto (nome, descricao) values ('Notebook','Notebook Gamming i7')",
-				Statement.RETURN_GENERATED_KEYS);
+		String sql = "insert into Produto (nome, descricao) values (?,?)";
+		PreparedStatement stmt = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
 
-		ResultSet resultSet = statement.getGeneratedKeys();
+		stmt.setString(1, nome);
+		stmt.setString(2, descricao);
+
+		boolean resultado = stmt.execute();
+
+		ResultSet resultSet = stmt.getGeneratedKeys();
 
 		while (resultSet.next()) {
 			int id = resultSet.getInt("id");
 			System.out.println(id + " gerado");
 		}
 
-		statement.close();
+		stmt.close();
 		connection.close();
 
 	}
