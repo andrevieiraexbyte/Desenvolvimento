30549c61c0894083e04bb3288ad75d86afde3532
André Vieira <andrevieira_@msn.com>
2018-03-07 18:41:56 -0300

    adicionando DriverManagerDataSource para conecxão

--- casadocodigo/src/main/java/br/com/casadocodigo/loja/conf/JPAConfiguration.java
+++ casadocodigo/src/main/java/br/com/casadocodigo/loja/conf/JPAConfiguration.java
@@ -1,6 +1,7 @@
 package br.com.casadocodigo.loja.conf;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.jpa.JpaVendorAdapter;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
@@ -17,6 +18,17 @@ public class JPAConfiguration {
 		JpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();// (63) crie um vendorAdapter
 		// (62) configurar factoryBean com vendoAdapter
 		factoryBean.setJpaVendorAdapter(vendorAdapter);// (64) passe como parametro o vendorAdapter
+
+		// (65) gerencia senha e usuario para o banco de dados
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		// (66) informando o usuario do banco de dados.
+		dataSource.setUsername("root");
+		// (67) informando senha
+		dataSource.setPassword("");
+		// (68) infromando url de conecxão
+		dataSource.setUrl("jdbc:mysql://localhost:3306/casadocodigo");
+		// (69) informando a classe do driver mysql para a conecxão
+		dataSource.setDriverClassName("com.mysql.jdbc.Driver");
 
 	}
 }
